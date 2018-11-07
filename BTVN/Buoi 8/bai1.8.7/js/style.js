c1274ec40d0fd52b2c3700027889ce230c0e5836
Long <nguyenlong30111999@gmail.com>
2018-11-08 02:03:02 +0700

    2h02 ngày 8/11/2018

---
+++ BTVN/Buoi 8/bai1.8.7/js/style.js
@@ -0,0 +1,15 @@
+<hr>
+	<button onclick="bai7()">Bài 7</button>
+	<script type="text/javascript">
+	function bai7(){
+		  var count=0;
+	        for (var i = 1; i < 100; i++) {
+	            if(i%3==0||i%7==0){
+	                count++;
+	            }
+	        }
+	        alert('Số lượng các số chia hết cho 3 hoặc 7 trong khoảng 1 đến 100 là: '+count);
+	    }
+	
+	</script> -->
+	<hr>
